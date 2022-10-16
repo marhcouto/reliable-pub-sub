e5c792c2c7e980748587e3b63bbc8cc36c0c7093
Marcelo Couto <up201906086@up.pt>
2022-10-16 23:13:17 +0100

    Finished basic client program, needs testing

--- client/src/main.rs
+++ client/src/main.rs
@@ -3,7 +3,9 @@ use std::process;
 
 mod subscriber;
 mod publisher;
-mod parsing;
+
+pub const USAGE_MESSAGE: &str = "USAGE:\ncargo run <service> <service_number> \nservice: name of the service to run. Can be one of subscribe or publisher\n
+service_number: number of the function to be executed.";
 
 fn main() {
 
@@ -11,7 +13,7 @@ fn main() {
 
     if args.len() < 3 {
         println!("Incorrect usage of the command line interface: missing arguments.");
-        println!("{}", parsing::USAGE_MESSAGE);
+        println!("{}", USAGE_MESSAGE);
         process::exit(1);
     }
 
@@ -21,13 +23,13 @@ fn main() {
     match service.as_str() {
         "subscriber" => {
             match service_number {
-                1 => subscriber::subscriber1(&args),
+                1 => subscriber::subscriber1(),
                 _ => panic!("Function subscriber{} does not exist", service_number)
             }
         },
         "publisher" => {
             match service_number {
-                1 => publisher::publisher1(&args),
+                1 => publisher::publisher1(),
                 _ => panic!("Function publisher{} does not exist", service_number)
             }
         },
