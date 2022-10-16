df96b3db519150816ecc2fd20c4ee5e779ccced4
Marcelo Couto <up201906086@up.pt>
2022-10-16 13:57:37 +0100

    Started client program

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,12 +1,10 @@
-#![allow(unused_imports)]
-use meic_mq::messages;
-use meic_mq::messages::NetworkTradeable;
-use meic_mq::context::subscriber;
-use meic_mq::context::publisher;
-
-use bson;
-use::std::env;
+use std::env;
+use std::process;
 use serde::{Serialize, Deserialize};
+
+mod subscriber;
+mod publisher;
+mod parsing;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Person {
@@ -16,27 +14,19 @@ struct Person {
 
 fn main() {
 
-//    let usage_message: String = String::from("USAGE:\ncargo run <service>\nservice: name of the service to run. Can be one of subscriber or publisher.");
-//    let args: Vec<String> = env::args().collect();
-//
-//    if args.len() < 2 {
-//        println!("Incorrect usage of the command line interface: missing arguments.");
-//        println!("{}", usage_message);
-//    }
+    let args: Vec<String> = env::args().collect();
 
+    if args.len() < 2 {
+        println!("Incorrect usage of the command line interface: missing arguments.");
+        println!("{}", parsing::USAGE_MESSAGE);
+        process::exit(1);
+    }
 
-    println!("CLIENT!");
+    let service: &String = &args[1];
 
-
-
-    /*let person = Person {
-        name: "Francisco Oliveira".to_string(),
-        age: 18
-    };
-    let data = bson::to_vec(&person).unwrap();
-    let mut file = File::create("foo.txt").unwrap();
-    file.write_all(&data).unwrap();
-    file = File::open("foo.txt").unwrap();
-    let file_data: Person = bson::from_reader(BufReader::new(file)).unwrap();
-    dbg!(file_data);*/
+    match service.as_str() {
+        "subscriber" => subscriber::subscriber(&args),
+        "publisher" => publisher::publisher(&args),
+        _ => panic!("Unknown service string {}", service)
+    }
 }
---
+++ client/src/parsing.rs
@@ -0,0 +1,3 @@
+
+
+pub const USAGE_MESSAGE: String = String::from("USAGE:\ncargo run <service>\nservice: name of the service to run. Can be one of subscriber or publisher.");
